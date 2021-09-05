5f65f71d2d66105b2d047031930fa1b6ebe741c3
Turi <40395348+turinaf@users.noreply.github.com>
2021-09-05 11:55:53 +0800

    Bottom TabBar with bottomNavigationBar

--- meals_app/lib/screens/tabs_screen.dart
+++ meals_app/lib/screens/tabs_screen.dart
@@ -10,33 +10,87 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  // functions for bottom navigation bar
+
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoriteScreen(),
+      'title': 'Your Favorite',
+    }
+  ];
+
+  int _selectedPageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0, // 0 is the default initail index.
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meal'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: "Favorite",
-              ),
-            ],
+    // -- TOP TabBar
+    // return DefaultTabController(
+    //   length: 2,
+    //   initialIndex: 0, // 0 is the default initail index.
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Meal'),
+    //       bottom: TabBar(
+    //         tabs: [
+    //           Tab(
+    //             icon: Icon(Icons.category),
+    //             text: "Categories",
+    //           ),
+    //           Tab(
+    //             icon: Icon(Icons.star),
+    //             text: "Favorite",
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(
+    //       children: [
+    //         CategoriesScreen(),
+    //         FavoriteScreen(),
+    //       ],
+    //     ),
+    //   ),
+    // );
+
+    // -- BOTTOM TabBar with bottomNavigationBar
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title'] as String),
+      ),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // Shifting changes the style of bottom naviagtion bar. And you may need to style NavigationBar items individually.
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(
+              Icons.category,
+            ),
+            label: "Categories",
           ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(
+              Icons.star,
+            ),
+            label: "Favorite",
+          ),
+        ],
       ),
     );
   }
