c109d1365e7417f1bc8bbb277343fb76876e9146
Turi <40395348+turinaf@users.noreply.github.com>
2021-09-25 10:47:09 +0800

    Alert dialog in Dismissible Widget

--- shop_app/lib/widgets/cart_item.dart
+++ shop_app/lib/widgets/cart_item.dart
@@ -33,6 +33,27 @@ class CartItem extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove Item from Cart?'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(false);
+                  },
+                  child: Text('No')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(true);
+                  },
+                  child: Text('Yes')),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         // remove the item, we don't need to listen to the change, becuase no need to rebuild cartItem.
         Provider.of<Cart>(context, listen: false).removeItem(productId);
