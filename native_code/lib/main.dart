fdc22b44346c7892418166b1c25a4e14422abe60
Turi <40395348+turinaf@users.noreply.github.com>
2022-02-03 11:50:26 +0800

    Running native code

---
+++ native_code/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Native Code'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int? _batteryLevel;
+Future<void> _getBatteryLevel () async{
+  const platform = MethodChannel('course.flutter.dev/battery');
+  try {
+    final batteryLevel = await platform.invokeMethod(
+        'getBatteryLevel'); // Invokes method in native code
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  } on PlatformException catch(error){
+    setState(() {
+     _batteryLevel = null;
+    });
+  }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Battery Level: $_batteryLevel'),
+      ),
+       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
